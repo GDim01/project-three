b3eba36a4f59dcca19e81747c8c34aa5c462aa12
GDim01 <georgedimitrov86@gmail.com>
2023-01-22 16:22:30 +0000

    Create function to calculate average speed

--- run.py
+++ run.py
@@ -11,11 +11,10 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('speed_survey')
+SURVEY_DATA = SHEET.worksheet('survey_data')
+DATA = SURVEY_DATA.get_all_values()
 
-survey_data = SHEET.worksheet('survey_data')
-
-data = survey_data.get_all_values()
-operational_data = data
+operational_data = DATA
 
 
 def function_select():
@@ -30,16 +29,15 @@ def function_select():
         selection = input("Enter your selection here:\n")
         if verify_selection(selection):
             print(f"You have selected program {selection}")
-            break
 
-    if selection == "1":
-        data_selection()
-    elif selection == "2":
-        pass
-    elif selection == "3":
-        pass
-    else:
-        pass
+        if selection == "1":
+            data_selection()
+        elif selection == "2":
+            average_speed()
+        elif selection == "3":
+            pass
+        else:
+            print("Exiting program")
 
 
 def verify_selection(selection):
@@ -53,41 +51,61 @@ def verify_selection(selection):
             raise ValueError(
                 f"Select a valid program 0-3. You entered {choice}"
             )
-    except ValueError as e:
-        print(f"Invalid data: {e}, please try again.\n")
+    except ValueError as error:
+        print(f"Invalid data: {error}, please try again.\n")
         return False
 
     return True
 
 
 def data_selection():
-    global data
+    """
+    Selects the relevant data chosen by the user and updates the
+    operational_data variable for use in other functions
+    """
+    global DATA
     global operational_data
     data_choice = input("Select the day to analyse 1-7 (0 for all)\n")
     if data_choice == "1":
         new_data = operational_data[0:2]
-        print(new_data)
+        operational_data = new_data
     elif data_choice == "2":
         new_data = operational_data[2:4]
-        print(new_data)
+        operational_data = new_data
     elif data_choice == "3":
         new_data = operational_data[4:6]
-        print(new_data)
+        operational_data = new_data
     elif data_choice == "4":
         new_data = operational_data[6:8]
-        print(new_data)
+        operational_data = new_data
     elif data_choice == "5":
         new_data = operational_data[8:10]
-        print(new_data)
+        operational_data = new_data
     elif data_choice == "6":
         new_data = operational_data[10:12]
-        print(new_data)
+        operational_data = new_data
     elif data_choice == "7":
         new_data = operational_data[12:14]
-        print(new_data)
+        operational_data = new_data
     else:
-        new_data = data
-        print(new_data)
+        new_data = DATA
+        operational_data = new_data
+    print(operational_data)
+
+
+def average_speed():
+    """
+    Calculates the average speed on the selected date
+    """
+    global operational_data
+    list_of_speeds = operational_data[1]
+    list_of_speeds_without_title = list_of_speeds[1:]
+    total_speed = 0
+    list_of_speeds_ints = [int(speed) for speed in list_of_speeds_without_title]
+    for speed in list_of_speeds_ints:
+        total_speed += speed
+    result = total_speed / len(list_of_speeds_ints)
+    print(f"The average speed during the selected date was {result}\n")
 
 
 def main():
